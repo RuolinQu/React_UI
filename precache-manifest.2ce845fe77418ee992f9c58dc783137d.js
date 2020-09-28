02c2233e1bf98132f69a38e4bf3522e53f408219
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-28 03:55:34 +0000

    Deploy RuolinQu/React_UI to github.com/RuolinQu/React_UI.git:gh-pages

---
+++ precache-manifest.2ce845fe77418ee992f9c58dc783137d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97ebd9e92ea92987500f854606c0aef",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.9da9f44b1b33a2ba05ff.bundle.js"
+  },
+  {
+    "url": "runtime~main.9da9f44b1b33a2ba05ff.bundle.js"
+  },
+  {
+    "revision": "79112b772ab000bfd1d0",
+    "url": "static/css/main.4cc840cc.chunk.css"
+  },
+  {
+    "revision": "caecc0041f427f708cc5",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.9da9f44b1b33a2ba05ff.bundle.js"
+  },
+  {
+    "url": "vendors~main.9da9f44b1b33a2ba05ff.bundle.js.LICENSE.txt"
+  }
+]);
